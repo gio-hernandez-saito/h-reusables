3ef06d4ca289a3a15a1079ee26d6cabedec512ac
Hyeonsik <gio.hernandez.saito@gmail.com>
2024-06-24 08:58:16 +0900

    Feat) UnoCSS 재시도

---
+++ src/atomics/atoms/button/HBaseButton.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="h-20 w-20 bg-pink"> button test </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import * as path from "node:path";
+import UnocssVitePlugin from "unocss/vite";
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts(), UnocssVitePlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')  // Add this alias
