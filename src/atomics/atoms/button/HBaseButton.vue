c152c1e845d513a44175f1abaa9507065a5fa912
Hyeonsik <gio.hernandez.saito@gmail.com>
2024-06-24 13:25:32 +0900

    Feat) Pure CSS로 스타일링 적용

--- src/atomics/atoms/button/HBaseButton.vue
+++ src/atomics/atoms/button/HBaseButton.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="h-base-btn">Hello</div>
+  <div class="h-base-btn">Button 2</div>
 </template>
 
 <script lang="ts" setup>
-
 </script>
-<style lang="sass">
+<style lang="sass" scoped>
 .h-base-btn
   width: 5rem
   height: 4rem
